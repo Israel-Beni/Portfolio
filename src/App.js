502c6663e38eaa833c15bc28b8cf61ea2339672b
Israel-Beni <bendegbe72@gmail.com>
2022-02-18 22:55:40 +0530

    Profile.js slate script written

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Profile from './Components/Profile/Profile';
 
 class App extends React.Component {
-
+  render() {
+    return (
+      <div className="app-container">
+        <Profile />
+      </div>
+    );
+  }
 };
 
 export default App;
---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Profile.css';
+import ProfileImage from '../../media/profile.jfif';
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <div className="component-container Profile-container">
+                <img className="profile-image" src={ProfileImage} alt="my profile" />
+                <div className="profile-info">
+                    <div className="profile-header">
+                        <div className="column1">
+                            <h1 className="name">Ben T. Degbe</h1>
+                            <p className="title">Front-end Developer</p>
+                        </div>
+                        <div className="column2">
+                            <p className="email">israelbeni71@gmail.com</p>
+                            <p className="contact-number">(+603) 546 624 342</p>
+                        </div>
+                    </div>
+                    <div className="description">
+                        <p>
+                            Self-motivated developer, who is willing to learn and create outstanding UI 
+                            applications.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Profile;
